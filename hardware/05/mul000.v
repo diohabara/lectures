dab787ea8ed90aafba71b2f73d0a1d9f0eb24ac6
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-04-22 18:27:07 +0900

    done until 05

--- hardware/05/mul000.v
+++ hardware/05/mul000.v
@@ -16,11 +16,26 @@ module mul(A,
     
     always @(posedge ck) begin
         if (start == 1) begin
-            O <= 0;
-            end else begin
-            O <= (O << 1) + A * B[7-st];
+            st  <= 0;
+            fin <= 0;
+            AIN <= A;
+            BIN <= B;
+            O   <= 0;
+        end
+        else begin
+            case (st)
+                0: O <= (O<<1) + AIN * BIN[7];
+                1: O <= (O<<1) + AIN * BIN[6];
+                2: O <= (O<<1) + AIN * BIN[5];
+                3: O <= (O<<1) + AIN * BIN[4];
+                4: O <= (O<<1) + AIN * BIN[3];
+                5: O <= (O<<1) + AIN * BIN[2];
+                6: O <= (O<<1) + AIN * BIN[1];
+            7: begin O <= (O<<1) + AIN * BIN[0]; fin <= 1; end
+            8: fin <= 0;
+            endcase
+            st <= st + 1;
         end
     end
     
 endmodule
-    
